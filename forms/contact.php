2e83a50c5a368a9fdd8221177e5fe5aa21a611ec
Indrajeet Javeri <indrajeet.javeri3@gmail.com>
2021-05-29 21:32:46 -0400

    Pre-Final draft :beers:

--- forms/contact.php
+++ forms/contact.php
@@ -5,7 +5,7 @@
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Server Error! Please try again later.');
   }
 
   $contact = new PHP_Email_Form;
